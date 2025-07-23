ef7968ff5eaad9a6052a8c432ca4a46dbf3f4940
LuizOrla <luiz.orlando2@gmail.com>
2025-07-23 14:52:03 -0300

    meu projeto conta banco

---
+++ src/main/java/ContaBanco/ContaTerminal.java
@@ -0,0 +1,14 @@
+
+
+package ContaBanco;
+
+/**
+ *
+ * @author luizo
+ */
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
